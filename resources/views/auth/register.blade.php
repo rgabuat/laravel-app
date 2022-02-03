2e8776efb9ba7a1f1c0c21f6e64360a92e6a68ab
unknown <Ellick BPO Solutions@PRD-AGT-013.ellickbpo.com>
2022-02-04 00:33:42 +0800

    first commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+
+@section('content')
+<div class="container">
+  <div class="col-12 shadow p-3 mb-5 bg-white rounded my-5">
+      <p>Register</p>
+
+      <form action="" method="post">
+          
+      </form>
+  </div>
+</div>
+@endsection
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+
+@section('content')
+<div class="container">
+  <div class="col-12 shadow p-3 mb-5 bg-white rounded my-5">
+      <p>Posts</p>
+  </div>
+</div>
+@endsection
