b69a82faae5f5ef866706cb02df43e359b85f24a
rgabuat <johncarlgabuat@gmail.com>
2022-02-04 01:09:23 +0800

    2nd commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+
+@section('content')
+<div class="container">
+  <div class="col-12 shadow p-3 mb-5 bg-white rounded my-5">
+      <p>login</p>
+
+    
+  </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+
 
 
 Route::get('/register',[RegisterController::class,'index'])->name('register');
+Route::get('/login',[LoginController::class,'index'])->name('login');
 
 Route::get('/posts', function () {
     return view('posts.index');
